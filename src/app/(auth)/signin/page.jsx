35dd54ca94b1c615bc19ccdc90d2b66c7373db5f
Yan Felipe <yanfelipe9@hotmail.com>
2025-02-20 22:58:46 -0300

    Login Page ajuste

--- src/app/(auth)/signin/page.jsx
+++ src/app/(auth)/signin/page.jsx
@@ -1,38 +1,25 @@
-import styles from './signin.module.css'
-import logo from './d78db4d398303024c2fb933c6c51f79e 1 (Traced).png'
-import Image from 'next/image'
-import { Label } from '@/components/Label'
-import { Button } from '@/components/Button'
-import { ArrowFoward } from '@/components/icons/ArrowFoward'
-import { TextDivider } from '@/components/TextDivider'
-import { Providers } from '@/components/Providers'
-import Link from 'next/link'
-import { Login } from '@/components/icons/Login'
-import { Checkbox } from '@/components/chekBox'
-import { createUser } from '@/actions'
-import { Input } from '@/components/input/input'
-import FormLogin from '@/components/FormLogin/FormLogin'
+import styles from './signin.module.css';
+import logo from './d78db4d398303024c2fb933c6c51f79e 1 (Traced).png';
+import Image from 'next/image';
+import FormLogin from '@/components/FormLogin/FormLogin';
+import { Col, Row } from 'antd';
+
 export default function Signin() {
   return (
     <main className={styles.main}>
       <div className={styles.container_left}>
-        <h1>
+        <div className={styles.container_left_title}>
           <Image src={logo} alt="Logo" className={styles.logo} />
-          PetHost
-        </h1>
-        <h2>Seu pet merece o melhor descanso, onde ele também se sente em casa!</h2>
+          <h1>
+            PetHost
+          </h1>
+        </div>
+        <h2 className={styles.h2}>Seu pet merece o melhor descanso, onde ele também se sente em casa!</h2>
       </div>
+
       <div className={styles.container}>
-        <h1>Login</h1>
-        <h2>Boas-vindas! Faça seu login.</h2>
         <FormLogin />
-        
-        <footer className={styles.footer}>
-          <p>
-            Não possui conta? <Link href='/signon'> Cadastre-se</Link>
-          </p>
-        </footer>
       </div>
     </main>
-  )
+  );
 }
--- src/components/FormLogin/FormLogin.jsx
+++ src/components/FormLogin/FormLogin.jsx
@@ -1,71 +1,58 @@
 'use client'
 
+import React, { useState } from 'react';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button,Form, Input, Flex, Typography } from 'antd';
+import { signIn } from 'next-auth/react';
+import Link from 'next/link'
 import styles from './form-login.module.css'
-import { Label } from '@/components/Label'
-import { Button } from 'antd'
-import { useState } from 'react'
-import { Input } from '../input/input'
-import { signIn } from 'next-auth/react'
+const { Title } = Typography;
+
 
 export default function FormLogin() {
+  const [loading, setLoading] = useState(false);
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  const onFinish = async (values) => {
+    setLoading(true);
+    await signIn('credentials', {
+      callbackUrl: '/',
+      email: values.email,
+      password: values.password,
+    });
+    setLoading(false);
+  };
 
-    const loginAttempt = async (event) => {
-
-        event.preventDefault()
-        signIn("credentials", {
-            callbackUrl : "/",
-            email,
-            password
-        })
-    }
-
-    return (
-        <form className={styles.form} onSubmit={loginAttempt}>
-            <div>
-                <Label>
-                    E-mail
-                </Label>
-                <Input
-                    name="email"
-                    id="email"
-                    type="email"
-                    placeholder="Digite seu e-mail"
-                    required
-                    onChange={event => setEmail(event.target.value)}
-                    value={email}
-                />
-            </div>
-            <div>
-                <Label>
-                    Senha
-                </Label>
-                <Input
-                    name="password"
-                    id="password"
-                    type="password"
-                    required
-                    placeholder="Digite sua Senha"
-                    onChange={event => setPassword(event.target.value)}
-                    value={password}
-                />
-            </div>
-            <div className={styles.action}>
-                <button 
-                    type='primary'
-                    htmlType='submit'
-                    className={styles.buttonCustom}>
-                    Entrar
-                </button>
-                {/* <Button
-                    type="primary"
-                    htmlType="submit"
-                >
-                    Entrar
-                </Button>       */}
-            </div>
-        </form>
-    )
+  return (
+    <Form
+      name="login"
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+    >
+      <Title level={3} style={{ textAlign: 'left' }}>Login</Title>
+      <Form.Item
+        name="email"
+        rules={[{ required: true, message: 'Digite seu e-mail!' }]}
+      >
+        <Input prefix={<UserOutlined />} placeholder="E-mail" type="email" />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[{ required: true, message: 'Digite sua senha!' }]}
+      >
+        <Input prefix={<LockOutlined />} type="password" placeholder="Senha" />
+      </Form.Item>
+      <Form.Item>
+        <Button block type="primary" htmlType="submit" loading={loading}>
+          Entrar
+        </Button>
+      </Form.Item>
+      <Form.Item>
+        <Flex justify="center">
+          <p className={styles.link}>
+            Não possui conta? <Link  href='/signon'> Cadastre-se</Link>
+          </p>
+        </Flex>
+      </Form.Item>
+    </Form>
+  );
 }
