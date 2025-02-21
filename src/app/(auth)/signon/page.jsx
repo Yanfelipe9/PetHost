262103db3cc855538be8bcd53aef9f6ff3e98b10
Yan Felipe <yanfelipe9@hotmail.com>
2025-02-20 23:14:23 -0300

    ajustando página Cadastro

--- src/app/(auth)/signon/page.jsx
+++ src/app/(auth)/signon/page.jsx
@@ -1,74 +1,22 @@
-import Image  from 'next/image'
-import logo from './d78db4d398303024c2fb933c6c51f79e 1 (Traced).png'
-import styles from './signon.module.css'
-import { Label } from '@/components/Label'
-import { Button } from '@/components/Button'
-import { ArrowFoward } from '@/components/icons/ArrowFoward'
-import { TextDivider } from '@/components/TextDivider'
-import { Providers } from '@/components/Providers'
-import Link from 'next/link'
-import { Login } from '@/components/icons/Login'
-import { Checkbox } from '@/components/chekBox'
-import { createUser } from '@/actions'
-import { Input } from '@/components/input/input'
-export default async function SignOn() {
+import styles from './signon.module.css';
+import logo from './d78db4d398303024c2fb933c6c51f79e 1 (Traced).png';
+import Image from 'next/image';
+import FormCadastro from '@/components/FormCadastro/FormCadastro'; // Alterado para FormCadastro
+
+export default function Signon() {
   return (
     <main className={styles.main}>
       <div className={styles.container_left}>
-        <h1><Image src={logo} alt="Logo PetHost" style={styles.logo} />PetHost</h1>
-        <h2>Seu pet merece o melhor descanso, onde ele também se sente em casa!</h2>
+        <div className={styles.container_left_title}>
+          <Image src={logo} alt="Logo" className={styles.logo} />
+          <h1>PetHost</h1>
+        </div>
+        <h2 className={styles.h2}>Seu pet merece o melhor descanso, onde ele também se sente em casa!</h2>
       </div>
+
       <div className={styles.container}>
-        <h1>
-          Cadastro
-        </h1>
-        <h2>
-          Olá! Preencha seus dados.
-        </h2>
-        <form className={styles.form} action={createUser}>
-          <div>
-            <Label>
-              Nome
-            </Label>
-            <Input 
-              name="name" 
-              id="name" 
-              placeholder="Nome completo" 
-              required 
-            />
-          </div>
-          <div>
-            <Label>
-              E-mail
-            </Label>
-            <Input 
-              name="email" 
-              id="email" 
-              type="email" 
-              placeholder="Digite seu e-mail" 
-              required 
-            />
-          </div>
-          <div>
-            <Label>
-              Senha
-            </Label>
-            <Input 
-              name="password" 
-              id="password" 
-              type="password" 
-              placeholder="Digite sua senha"
-              required 
-            />
-          </div>
-          <div className={styles.action}>
-            <Button type="submit">
-              Cadastrar 
-            </Button>
-          </div>
-        </form>
-        
+        <FormCadastro />
       </div>
     </main>
-  )
+  );
 }
