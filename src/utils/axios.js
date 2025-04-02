3e659ebfdbce08eaccf8e1d5d69121a659c2029f
unknown <georgefelipe938@gmail.com>
2025-04-02 00:03:42 -0300

    interceptor de erro e acesso

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { message } from 'antd';
 
 // Obtendo a URL do backend a partir do ambiente
 const API_URL = process.env.NEXT_PUBLIC_BACK_URL || 'http://localhost:8080';
@@ -23,6 +24,46 @@ api.interceptors.request.use(
     return config;
   },
   (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => response, 
+  (error) => {
+    if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+    
+      localStorage.clear();
+  
+      if (typeof window !== 'undefined') {
+        window.location.href = '/signin';
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const status = error.response?.status;
+
+    // Redirecionamento no caso de 401/403
+    if (status === 401 || status === 403) {
+      localStorage.clear();
+      if (typeof window !== 'undefined') {
+        window.location.href = '/signin';
+      }
+    }
+
+    // Exibe mensagem de erro global
+    const errorMsg =
+    error.response?.data?.message ||
+      error.response?.data?.error ||
+      'Ocorreu um erro inesperado.';
+
+    message.error(errorMsg, 5); // 5 segundos de duração
+
+    return Promise.reject(error);
+  }
 );
 
 
