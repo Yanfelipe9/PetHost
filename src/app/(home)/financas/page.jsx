0c3ac4032695ae24e9a6fd854ccbd7d2a6886823
Ronaldo-j <ronaldo00filho@gmail.com>
2025-04-12 16:25:38 -0300

    Acrescentando o grafico de barras

--- src/app/(home)/financas/page.jsx
+++ src/app/(home)/financas/page.jsx
@@ -1,12 +1,15 @@
 'use client'
-import React, { useEffect, useState } from "react";
-import { Table, Input, Button, Space, Flex, Tabs, message, Spin } from "antd";
+import React, { useEffect, useRef, useState } from "react";
+import { Table, Input, Button, Space, Flex, Tabs, message, Spin, Card, Typography } from "antd";
 import { SearchOutlined, FilterOutlined } from "@ant-design/icons";
 import styles from './financas.module.css';
 import api from "@/utils/axios";
 import { useAuth } from "@/app/context/AuthContext";
+import ApexCharts from "apexcharts";
+
 
 const { TabPane } = Tabs;
+const { Title } = Typography;
 
 const FinanceiroPage = () => {
   const { user } = useAuth();
@@ -14,6 +17,7 @@ const FinanceiroPage = () => {
   const [agendamentos, setAgendamentos] = useState([]);
   const [loading, setLoading] = useState(false);
   const [activeTab, setActiveTab] = useState("financas");
+  const chartRef = useRef(null);
 
   useEffect(() => {
     if (!user?.userId) return;
@@ -32,6 +36,67 @@ const FinanceiroPage = () => {
 
     fetchAgendamentos();
   }, [user?.userId]);
+
+  useEffect(() => {
+    let chart;
+  
+    if (activeTab === "painel") {
+      setTimeout(() => {
+        if (chartRef.current) {
+          const options = {
+            series: [
+              { name: 'Entradas', data: [44, 55, 57, 56, 61, 58, 63, 60, 66, 53, 75, 10] },
+              { name: 'Despesas', data: [76, 85, 101, 98, 87, 105, 91, 114, 94, 21, 54, 90] },
+            ],
+            chart: {
+              type: 'bar',
+              height: 350
+            },
+            plotOptions: {
+              bar: {
+                horizontal: false,
+                columnWidth: '55%',
+                borderRadius: 5,
+                borderRadiusApplication: 'end'
+              },
+            },
+            dataLabels: { enabled: false },
+            stroke: {
+              show: true,
+              width: 2,
+              colors: ['transparent']
+            },
+            xaxis: {
+              categories: ['JAN', 'FEV', 'MAR', 'ABRIL', 'MAI', "JUN", 'JUL', 'AGO', 'SET', 'OUT', 'NOV', 'DEZ'],
+            },
+            yaxis: {
+              title: {
+                text: '$ (thousands)'
+              }
+            },
+            fill: {
+              opacity: 1
+            },
+            tooltip: {
+              y: {
+                formatter: function (val) {
+                  return "$ " + val + " thousands"
+                }
+              }
+            }
+          };
+  
+          chart = new ApexCharts(chartRef.current, options);
+          chart.render();
+        }
+      }, 100); // espera 100ms até o DOM estar pronto
+    }
+  
+    return () => {
+      if (chart) chart.destroy();
+    };
+  }, [activeTab]);
+  
 
   const filteredData = agendamentos.filter(
     (item) =>
@@ -53,23 +118,25 @@ const FinanceiroPage = () => {
       <Tabs activeKey={activeTab} onChange={(key) => setActiveTab(key)} style={{ marginBottom: 16 }}>
         <TabPane tab="Painel" key="painel">
 
-          {/* grafico de barra   */}
-          <Card className={styles.card}>
-            <Title level={5}>Ultimas entradas/saidas</Title>
-            <p>Resumo das despesas de Jan a Dez</p>
-            {/*grafico que vai ser inserido <div ref={chartRefBar}></div> */}
-            <div style={{ display: "flex", gap: "8px", fontSize: "14px" }}>
-              <div style={{ display: "flex", alignItems: "center", gap: "6px" }}>
-                <span style={{ width: "10px", height: "10px", backgroundColor: "#3b82f6", borderRadius: "50%" }}></span>
-                <span style={{ color: "#6b7280" }}>Entradas</span>
-              </div>
-              <div style={{ display: "flex", alignItems: "center", gap: "6px" }}>
-                <span style={{ width: "10px", height: "10px", backgroundColor: "#e5e7eb", borderRadius: "50%" }}></span>
-                <span style={{ color: "#6b7280" }}>Despesas</span>
-              </div>
-            </div>
+          <Card>
+            <h3>Saldo Liquido</h3>
+            <Title>$3120.54</Title>
           </Card>
-        </TabPane> 
+          <Card>
+            <h3>Entrada</h3>
+            <Title>$3120.54</Title>
+          </Card>
+          <Card>
+            <h3>Despesas</h3>
+            <Title>$3120.54</Title>
+          </Card>
+          {/* Grafico de barra de entradas e despesas */}
+          <Card className={styles.card}>
+            <Title level={5}>Últimas entradas/saídas</Title>
+            <p>Resumo das despesas de Jan a Dez</p>
+            <div ref={chartRef} style={{ minHeight: 350 }} />
+          </Card>
+        </TabPane>
         <TabPane tab="Finanças" key="financas">
           <Flex justify="space-between" align="flex-start" className={styles.header}>
             <Space style={{ marginBottom: 16 }}>
